1146243f9496620a571b1e3b4e88eb3614b6dcd3
Juniper Lake <juniper.a.lake@gmail.com>
2021-07-10 13:29:28 -0500

    more parameters and better output file

--- stepwiseSVM.R
+++ stepwiseSVM.R
@@ -3,36 +3,68 @@
 ##############################################################################
 # Help message for command line usage
 ##############################################################################
-
+options(warning.length = 5000L) #stop error messages (including help file) from getting truncated.
 help <- "
-Usage: stepwiseSVM.R [options]
+The program `stepwiseSVM.R` constructs a support vector machine (SVM) 
+with stepwise feature selection (forward and backward) using leave-one-out 
+cross-validation.
+
+It  requires R-4.1.0 to run and can take a lot of memory for iterations, 
+so it was designed to be run on the Biomix cluster with the wrapper shell
+script `run_stepwiseSVM.job`, which checks for installation of R-4.1.0, 
+adjusts R memory limits using `ulimit`, and allows for the Rscript parameters
+to be adjusted in the section labeled #VARIABLES YOU CAN CHANGE.
+
+The first time running this script may take a while due to installation of
+R-4.1.0 (if using the wrapper shell script) and/or installation of the required
+R packages. However, subsequent runs should be shorter if you continue to use the 
+same directory because the wrapper shell script will check to see if R-4.1.0 is
+isntalled in that directory and the R script will check to see if the packages 
+are already installed in --libpath.
+
+Usage: Rscript stepwiseSVM.R [options]
 
 Examples:
 Rscript stepwiseSVM.R --help
-Rscript stepwiseSVM.R --data=test_data.csv --libPath=../svm_libraries
+Rscript stepwiseSVM.R --data=test_data.csv --libPath=./svm_libraries
 
 Options:
   --help
     Show this help message and exit
     
   --inputFile=CHARACTER
-    A single comma delimited text file containing phenotypes in the first column \n
+    A single comma delimited text file containing phenotypes in the first column 
     and features in the remaining columns. The first row should be column names.
+  
+  --outputFile=CHARACTER
+    An output file name that does not match any existing filename in the output 
+    directory. This program WILL NOT overwrite existing files, it will simply 
+    stop running and show this help file. By default, results are written to 
+    'results_stepwiseSVM.txt'
     
   --libPath=CHARACTER
-    The pathway to libraries required for this analysis. If you have run this \n
-    analysis before and already have libraries installed, specifying where those \n
-    libraries are installed can speed up the analysis time and prevent them from \n
-    being re-installed. By default, the program looks for a directory called \n
-    'svm_libraries' in the working directory. If 'svm_libraries' is missing it will be \n
-    created.\n
+    The pathway to libraries required for this analysis. If you have run this 
+    analysis before and already have libraries installed, specifying where those 
+    libraries are installed can speed up the analysis time and prevent them from 
+    being re-installed. By default, the program looks for a directory called 
+    'svm_libraries' in the working directory. If 'svm_libraries' is missing it 
+    will be created.
+  
+  --cost=NUMBER
+    Parameter for linear SVM as implemented by 'kernlab'. Cost of constraints 
+    violation (default: 1) this is the `C'-constant of the regularization term 
+    in the Lagrange formulation.
+    
+  --maxIterations=INTEGER
+    The maximum number of iterations that will be run before model training
+    automatically terminates.\n
 "
 
 # These are the possible options (not including help). Make sure they are consistent 
 # with the help page. The flag inputFile should not have a default.
-possible_args <- c("inputFile", "libPath")
+possible_args <- c("inputFile", "libPath", "outputFile", "cost", "maxIterations")
 required_args <- c("inputFile")
-defaults <- list(libPath="../svm_libraries")
+defaults <- list(libPath="./svm_libraries", outputFile="results_stepwiseSVM.txt", cost=1, maxIterations=100)
 
 ##############################################################################
 # Read in arguments from command line
@@ -81,7 +113,22 @@ for (arg in default_needed){
   args_list[[arg]]=defaults[[arg]]
 }
 
-# list arguments used for posterity
+# make sure output file doesn't already exist to avoid overwriting
+if (file.exists(args_list$outputFile)){
+  stop("The output file ", args_list$outputFile, " already exists. Please specify an outputFile name that does not exist. See help page below.\n", help, call.=FALSE)
+}
+
+# make sure cost parameter is a number
+if(is.na(as.double(args_list$cost))){
+  stop("The cost parameter you entered", args_list$cost, " is not a number. Please specify a valid cost. See help page below.\n", help, call.=FALSE)
+}
+
+# make sure maxIterations parameter is a number, if not an integer, then coerce as integer
+if(is.na(as.integer(args_list$maxIterations))){
+  stop("The maxIterations parameter you entered", args_list$maxIterations, " is not a number. Please specify a valid maxIterations See help page below.\n", help, call.=FALSE)
+}
+
+# list arguments in console for posterity
 cat("\nThe following parameters are being used in this analysis:\n")
 for (arg in names(args_list)){
   cat(paste0(arg, ": ", args_list[[arg]], "\n"))
@@ -142,13 +189,13 @@ packages <- list(CombMSC="https://cran.r-project.org/src/contrib/CombMSC_1.4.2.1
                  ipred="https://cran.r-project.org/src/contrib/ipred_0.9-11.tar.gz",
                  timeDate="https://cran.r-project.org/src/contrib/timeDate_3043.102.tar.gz",
                  generics="https://cran.r-project.org/src/contrib/generics_0.1.0.tar.gz", 
+                 purrr="https://cran.r-project.org/src/contrib/purrr_0.3.4.tar.gz", 
+                 tidyselect="https://cran.r-project.org/src/contrib/tidyselect_1.1.1.tar.gz",
                  plyr="https://cran.r-project.org/src/contrib/plyr_1.8.6.tar.gz",
                  dplyr="https://cran.r-project.org/src/contrib/dplyr_1.0.7.tar.gz", 
                  lubridate="https://cran.r-project.org/src/contrib/lubridate_1.7.10.tar.gz", 
-                 purrr="https://cran.r-project.org/src/contrib/purrr_0.3.4.tar.gz", 
                  cpp11="https://cran.r-project.org/src/contrib/cpp11_0.3.1.tar.gz",
                  tidyr="https://cran.r-project.org/src/contrib/tidyr_1.1.3.tar.gz", 
-                 tidyselect="https://cran.r-project.org/src/contrib/tidyselect_1.1.1.tar.gz",
                  stringi="https://cran.r-project.org/src/contrib/stringi_1.6.2.tar.gz",
                  stringr="https://cran.r-project.org/src/contrib/stringr_1.4.0.tar.gz",
                  recipes="https://cran.r-project.org/src/contrib/recipes_0.1.16.tar.gz",
@@ -167,104 +214,128 @@ for (package in missing_packages){
 invisible(lapply(names(packages), require, character.only = TRUE, lib.loc=library_location))
 
 ##############################################################################
-# Data Preprocessing
+# Open results file
+##############################################################################
+cat("\nRequired packages are loaded and SVM construction will begin. Results will be logged in ", args_list$outputFile, "\n")
+
+out_file<-args_list$outputFile
+start <- "
+You are constructing a support vector machine (SVM) with stepwise feature selection (forward and backward) using leave-one-out cross-validation.\n\n"
+cat(start, file = out_file)
+
+# list arguments in outputFile for posterity
+cat("The following parameters are being used in this analysis:\n", file = out_file, append = T)
+for (arg in names(args_list)){
+  cat(c(arg, ": ", args_list[[arg]], "\n"), file = out_file, append = T)
+  cat("\n")
+}
+
+##############################################################################
+# Data Pre-processing
 ##############################################################################
 data_file <- args_list$inputFile
   
 data<-read.csv(data_file, header = T)
+data[,1] <- as.factor(data[,1])
 
-normal <-c()
-for (i in 2:616){
-  for (j in 1:246){
-    if (is.nan(data[j,i]) == FALSE){
-      data[j,i] <- log(data[j,i])
-    }
-  }
-}
-for (i in 2:616){
-  vmean<-mean(data[,i], na.rm = TRUE)
-  vstd <- sd(data[,i], na.rm = TRUE)
-  data[,i]<-(data[,i]-vmean) / vstd
-}
+cat(c("\nNumber of features in data file: ", ncol(data)-1, "\n"), file = out_file, append = T)
+cat(c("Number of samples in data file: ", nrow(data), "\n"), file = out_file, append = T)
+cat(c("Phenotype groups: ", paste0(levels(data[,1]), collapse = ", "), "\n\n"), file = out_file, append = T)
 
-list <- c()
-for (i in 2:616){
-  if (sum(is.na(data[,i])) / 246 > 0.6){
-    list <- c(list, i)
-  }
-}
-data_pro <- data[, -list]
+##############################################################################
+# Leave-one-out Cross-validation
+##############################################################################
 
-
-for (i in 2:length(data_pro)){
-  vmin<-min(data_pro[, i], na.rm=TRUE)
-  data_pro[is.na(data_pro[, i]),i]<-vmin
-}
-result<-data_pro
-result$category <- as.factor(result$category)
-
-
-##########################################################
-# leave one out###########################################
 train.control2 <- trainControl(method = "LOOCV",
                                search = "grid")
 
 
-##########################################################
-## stepwise SVM algorithm ################################
+##############################################################################
+# Stepwise SVM algorithm
+##############################################################################
 
+
+cost <- as.double(args_list$cost)
+max_iterations <- as.double(args_list$maxIterations)
 comb <- c()
-result.train1 <- result
-y_train <- result$category
+result.train1 <- data
+y_train <- data[,1]
 avg_bar <- 0
 accuracy_bar <- 0
 record <- c()
 selected <- c()
 
-cat("\nStarting model construction using stepwise selection.\n")
-for (i in 1:100){
-  cat("Iteration number: ", i, "\n")
-  len <- length(subsets(dim(result.train1)[2]-1,1,colnames(result.train1[,!(colnames(result.train1) == "category")])))
+cat("Starting model construction using stepwise selection.\n", file = out_file, append = T)
+for (i in 1:max_iterations){
+  cat(c("\nIteration number: ", i, "\n"), file = out_file, append = T)
+  
+  # Forward feature selection (find feature that improves accuracy the most)
+  len <- length(subsets(dim(result.train1)[2]-1,1,colnames(result.train1[,2:ncol(result.train1)])))
   for (j in 1:len){
-    set <- result[c(selected, subsets(dim(result.train1)[2]-1,1,colnames(result.train1[,!(colnames(result.train1) == "category")]))[j,])]
+    set <- data[c(selected, subsets(dim(result.train1)[2]-1,1,colnames(result.train1[,2:ncol(result.train1)]))[j,])]
     fit.svml <- train(set,
                       y_train,
                       method = "svmLinear",
-                      tuneGrid = expand.grid(C = 0.1),
+                      tuneGrid = expand.grid(C = cost),
                       trControl = train.control2)
     avg <- mean(fit.svml$results$Accuracy)
-    #acc <- c(acc, avg)
-
     if (avg > avg_bar){
       comb <- c(i, j, avg)
       avg_bar <- avg
     }
   }
-  select_var <- subsets(dim(result.train1)[2]-1,1,colnames(result.train1[,!(colnames(result.train1) == "category")]))[comb[2],]
+  select_var <- subsets(dim(result.train1)[2]-1,1,colnames(result.train1[,2:ncol(result.train1)]))[comb[2],]
   selected <- c(selected, select_var) # append variables
+  cat("The feature that improves model classification accuracy the most:\n", file = out_file, append = T)
+  cat(c("+ ", select_var, " | Mean Accuracy: ", avg_bar, "\n"), file = out_file, append = T)
   record <- rbind(record, c("+", select_var, avg_bar)) # add selected variable into our record
-  cat(record)
+
+  # Backward feature selection (check if removing any features from model will improve accuracy)
+  # WARNING: I'm not confident that this loop would ever actually remove variables from the model. I don't know if the removal criteria
+  # is adequate and I also feel like there should be a line after the `if (avg > avg_bar)` clause along the lines of `selected <- selected[-k]`
   for (k in 1: length(record[,1])){
     set1 <- as.data.frame(set[, -k])
     fit.svml <- train(set1,
                       y_train,
                       method = "svmLinear",
-                      tuneGrid = expand.grid(C = 0.1),
+                      tuneGrid = expand.grid(C = cost),
                       trControl = train.control2)
     avg <- mean(fit.svml$results$Accuracy)
     if (avg > avg_bar){
       avg_bar <- avg
+      cat("Removing the following feature improves model classification accuracy:\n", file = out_file, append = T)
+      cat(c("- ", selected[k], " | Mean Accuracy: ", avg_bar,"\n"), file = out_file, append = T)
       record <- rbind(record, c("-", selected[k], avg_bar)) # remove selected variable out of our record
-      cat(record)
-      result.train1[selected[k]]<-result[selected[k]]
+      result.train1[selected[k]]<-data[selected[k]]
     }
   }
-  result.train1 <- result.train1[,!(colnames(result.train1)==select_var)] # re-adjust the dataset dim
-  confusionMatrix(fit.svml$pred$pred, fit.svml$pred$obs)
+  
+  # Re-adjust dataset dimensions
+  result.train1 <- result.train1[,!(colnames(result.train1)==select_var)]
+  
+  # If the current iteration doesn't improve overall accuracy, then quit iterating and report last iteration's model
   if (i!=1 && avg_bar <= accuracy_bar){
     break
   }
   accuracy_bar <- avg_bar
+  
+  # Print confusion matrix for current iteration's model
+  fit.svml <- train(data[selected],
+                    y_train,
+                    method = "svmLinear",
+                    tuneGrid = expand.grid(C = cost),
+                    trControl = train.control2)
+  cat(c("\nFeatures in model: ", selected, "\n"), file = out_file, append = T)
+  cat("\n", file = out_file, append = T)
+  cm <- confusionMatrix(fit.svml$pred$pred, fit.svml$pred$obs)
+  capture.output(print(cm), file = args_list$outputFile, append =T)
 }
 
-cat("\nAnalysis complete.\n")
+cat(c("\nStepwise feature selection has completed because further changes do not improve classification accuracy. Final results are present under iteration number", i-1), file = out_file, append = T)
+
+# This prints the record, but it includes the changes made in the last iteration which are not included in the final model, so not sure if it's good to use
+# invisible(apply(record, 1, function(x) cat(c(x[1], " ", x[2], " | Mean Accuracy: ", x[3], "\n"), file = out_file, sep = "", append = T)))
+
+cat("\nAnalysis complete. Please see results in ", args_list$outputFile, "\n\n")
+
+
